01ad1544b7356a3b8c24722323b769f14e5687fb
krivera1015 <krivera1015@gmail.com>
2018-07-11 19:55:47 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push(array, string)
+  return array.push(string)
+end
+
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
+def using_shift(array)
+  return array.shift
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(array_one, array_two)
+  array_one.concat(array_two)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
